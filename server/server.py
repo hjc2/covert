a6fee50426c6dd1ad20401c14ff7e920aa797d55
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2024-07-26 20:22:51 -0400

    updated server

--- server/server.py
+++ server/server.py
@@ -29,22 +29,21 @@ def server_program():
             receive_data = conn.recv(1024)
             if b'%EOF' in receive_data:
                 file.write(receive_data)
+                file.close()
                 break
         
-        file.close()
-
         extracted_data = pdfcovert.extract_data_from_pdf('file_'+ str(i)+".pdf")
         print(extracted_data)
 
         hidden_data = input("Data to send -> ")
 
-        send_pdf_name = input("Name of pdf to send -> ")  # 
+        send_pdf_name = input("Name of pdf to send -> ") 
 
         pdfcovert.embed_data_in_pdf(send_pdf_name, "encrypted.pdf", hidden_data)
 
         pdf = open("encrypted.pdf", "rb")
         send_data = pdf.read(1024)
-        while not send_data.__contains__('%%EOF'):
+        while send_data:
             conn.send(send_data)
             send_data = pdf.read(1024)
         
